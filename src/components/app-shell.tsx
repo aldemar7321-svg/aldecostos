2bd8a0e49cb8187ae5e35a5cb6fbd017497f0cfc
ALDEMAR MORALES GUTIERREZ <aldemar7321@gmail.com>
2025-11-07 13:32:13 +0000

    Console Error
    In HTML, <a> cannot be a descendant of <a>.
    This will ca

--- src/components/app-shell.tsx
+++ src/components/app-shell.tsx
@@ -54,9 +54,8 @@ export default function AppShell({ children }: { children: React.ReactNode }) {
           <SidebarMenu>
             {navItems.map((item) => (
               <SidebarMenuItem key={item.href}>
-                <Link href={item.href} asChild>
+                <Link href={item.href}>
                   <SidebarMenuButton
-                    as="a"
                     isActive={pathname === item.href}
                     tooltip={item.label}
                   >
