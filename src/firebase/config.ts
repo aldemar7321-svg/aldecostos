6a7b2ac3f4871f14b0496a203062c7b7133afa3e
ALDEMAR MORALES GUTIERREZ <aldemar7321@gmail.com>
2025-11-25 15:12:35 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9012377657-b6963",
+  "appId": "1:1068859541360:web:b177b686cd9c85e4cef1b5",
+  "apiKey": "AIzaSyCr9P5rhtYawvnwoFVP4Sjf5iJxoKU-yxM",
+  "authDomain": "studio-9012377657-b6963.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1068859541360"
+};
